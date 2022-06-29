7b725109ea7acc2e0ab06a71b42411f525d6b2e3
Perkles <lucasbrittobr@gmail.com>
2022-06-29 16:16:52 -0300

    Javascript 1.1

---
+++ Seção 6 - Assíncrono/Ajax/assets/js/FetchAPI.js
@@ -0,0 +1,28 @@
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+    
+    if (tag === 'a') {
+      e.preventDefault();
+      carregaPagina(el);
+    }
+});
+    
+function carregaPagina(el) {
+    const href = el.getAttribute('href')
+
+    fetch(href)
+        .then(response => {
+            if (response.status !== 200) throw new Error('ERRO 404!')
+            return response.text()
+        })
+        .then(html => carregaResultado(html))
+        .catch(e => console.warn(e))
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.res');
+    resultado.innerHTML = response;
+}
+
+    
