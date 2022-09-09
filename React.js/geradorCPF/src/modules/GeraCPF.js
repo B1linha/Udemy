820d5049086cda2b8c15733bd5cdd7b62115d21b
lucasbarreto <99029800+B1linha@users.noreply.github.com>
2022-09-09 02:52:37 -0300

    React 1.4

---
+++ React.js/geradorCPF/src/modules/GeraCPF.js
@@ -0,0 +1,11 @@
+import ValidaCPF from "./ValidaCPF";
+
+export default class GeraCPF {
+    rand(min = 100000000, max = 999999999) {
+        return String(Math.floor(Math.random() * (max - min) + min));
+    }
+
+    geraNovoCPF() {
+        
+    }
+}
